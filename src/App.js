a8f31621c3f9c09cd9f2e10516b170a028079353
ThietVo <vonhuthiet@gmail.com>
2022-08-26 17:40:50 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import io from 'socket.io-client';
+
+// const socket = io.connect("http://127.0.0.1:5000");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const socket = io('localhost:5001/', {
+            transports: ['websocket'],
+            cors: {
+                origin: 'http://localhost:3000/',
+            },
+        });
+
+        socket.on('connect', (data) => {
+            console.log(data);
+        });
+    }, []);
+    return <div className="App"></div>;
 }
 
 export default App;
